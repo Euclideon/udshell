67610ebcb1a87ebca5a29d04f48fbc80150e10ae
Manu Evans <mevans@euclideon.com>
2015-06-23 18:29:03 +1000

    Update to latest UD

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,25 @@
 
 #include "quick/renderview.h"
 
+#include "udResult.h"
+#include "udKernel.h"
+
+udKernel *s_pKernel;
+
 int main(int argc, char *argv[])
 {
+  // unit test
+  udResult udSlice_Test();
+  udSlice_Test();
+  udResult udString_Test();
+  udString_Test();
+
+  // create a kernel
+  udResult r = udKernel::create(&s_pKernel, argc, argv, 8);
+  if (r == udR_Failure_)
+    return 1;
+
+  // run Qt app
   QGuiApplication app(argc, argv);
 
   QQmlApplicationEngine engine;
