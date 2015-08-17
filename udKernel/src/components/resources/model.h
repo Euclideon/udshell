5535b6044ec86c26fb6570e7815e3e79e02124a6
Manu Evans <mevans@euclideon.com>
2015-08-17 14:12:24 +1000

    Switched Model component to use enum data arrays.

--- udKernel/src/components/resources/model.h
+++ udKernel/src/components/resources/model.h
@@ -18,29 +18,37 @@ class Model : public Resource
 public:
   UD_COMPONENT(Model);
 
-  ArrayBufferRef GetPositionBuffer() const { return spPositions; }
+  UD_ENUM(ArrayType,
+            Indices,
+            Positions,
+            Normals,
+            BinormalsAndTangents,
+            UV0, UV1, UV2, UV3, UV4, UV5, UV6, UV7,
+            Color0, Color1, Color2, Color3, Color4, Color5, Color6, Color7,
+            Max
+          );
+
+  ArrayBufferRef GetArray(ArrayType array) const { return spArrays[array]; }
+  void SetArray(ArrayType array, ArrayBufferRef spArray) { spArrays[array] = spArray; }
 
   void SetMaterial(MaterialRef spMaterial) { this->spMaterial = spMaterial; }
 
-  void SetPositions(ArrayBufferRef spPositions) { this->spPositions = spPositions; }
-  void SetNormals(ArrayBufferRef spNormals) { this->spNormals = spNormals; }
-  void SetBinormalsAndTangents(ArrayBufferRef spBinormalsAndTangents) { this->spBinormalsTangents = spBinormalsAndTangents; }
-  void SetUVs(size_t uvChannel, ArrayBufferRef spUVs) { this->spUVs[uvChannel] = spUVs; }
-  void SetColors(size_t colorChannel, ArrayBufferRef spColors) { this->spColors[colorChannel] = spColors; }
+  ArrayBufferRef GetPositionBuffer() const { return spArrays[ArrayType::Positions]; }
 
-  void SetIndices(ArrayBufferRef spIndices) { this->spIndices = spIndices; }
+  void SetPositions(ArrayBufferRef spPositions) { spArrays[ArrayType::Positions] = spPositions; }
+  void SetNormals(ArrayBufferRef spNormals) { spArrays[ArrayType::Normals] = spNormals; }
+  void SetBinormalsAndTangents(ArrayBufferRef spBinormalsAndTangents) { spArrays[ArrayType::BinormalsAndTangents] = spBinormalsAndTangents; }
+  void SetUVs(size_t uvChannel, ArrayBufferRef spUVs) { spArrays[ArrayType::UV0 + uvChannel] = spUVs; }
+  void SetColors(size_t colorChannel, ArrayBufferRef spColors) { spArrays[ArrayType::Color0 + colorChannel] = spColors; }
+
+  void SetIndices(ArrayBufferRef spIndices) { spArrays[ArrayType::Indices] = spIndices; }
 
 protected:
   Model(const ComponentDesc *pType, Kernel *pKernel, udRCString uid, udInitParams initParams)
     : Resource(pType, pKernel, uid, initParams) {}
 
-  ArrayBufferRef spPositions = nullptr;
-  ArrayBufferRef spNormals = nullptr;
-  ArrayBufferRef spBinormalsTangents = nullptr;
-  ArrayBufferRef spUVs[8];
-  ArrayBufferRef spColors[8];
-  ArrayBufferRef spIndices = nullptr;
-  MaterialRef spMaterial = nullptr;
+  ArrayBufferRef spArrays[ArrayType::Max];
+  MaterialRef spMaterial;
 };
 
 } // namespace ud
