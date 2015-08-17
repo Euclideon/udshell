9027afa2543fd72b8306f7a421bf88e5cf43081a
David Ely <dely@euclideon.com>
2015-08-17 14:53:40 +1000

    Added NULL halTimer.

---
+++ udKernel/src/driver/Null/haltimer_Null.cpp
@@ -0,0 +1,14 @@
+#include "hal/driver.h"
+
+#if UDWINDOW_DRIVER == UDDRIVER_NULL
+
+HalTimer *HalTimer_Create(HalTimerType , uint32_t ,  HalTimerCallback *, void *)
+{
+  return nullptr;
+}
+
+void HalTimer_Destroy(HalTimer **)
+{
+}
+
+#endif // UDWINDOW_DRIVER == UDDRIVER_NULL
