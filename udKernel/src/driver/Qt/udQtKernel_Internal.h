b67f0758af6d2876362ff4a9cb3bd1db07615d75
Shane Bradley <sbradley@euclideon.com>
2015-08-18 16:17:24 +1000

    Split out Qt type conversion stuff to a separate file.

--- udKernel/src/driver/Qt/udQtKernel_Internal.h
+++ udKernel/src/driver/Qt/udQtKernel_Internal.h
@@ -6,7 +6,6 @@
 #include <QQmlEngine>
 #include <QThread>
 #include <QEvent>
-#include <QMetaType>
 
 #include "kernel.h"
 #include "ui/qtuicomponent.h"
@@ -56,102 +55,5 @@ private:
 };
 
 } // namespace qt
-
-// Qt type conversion to/from UD
-
-inline udString AllocUDStringFromQString(const QString &string)
-{
-  QByteArray byteArray = string.toUtf8();
-
-  // Need to do a deep copy and give ownership to the variant
-  size_t length = byteArray.size()+1;
-  char *pString = udAllocType(char, length, udAF_None);
-  memcpy(pString, byteArray.data(), length);
-
-  return udString(pString, length-1);
-}
-
-inline udVariant udToVariant(const QString &string)
-{
-  return udVariant(AllocUDStringFromQString(string), true);
-}
-
-inline void udFromVariant(const udVariant &variant, QString *pString)
-{
-  udString s = variant.asString();
-  if (!s.empty())
-    *pString = QString::fromUtf8(s.ptr, static_cast<int>(s.length));
-}
-
-inline udVariant udToVariant(const QVariant &var)
-{
-  switch (static_cast<QMetaType::Type>(var.type()))
-  {
-    // udVariant::Type::Bool
-    case QMetaType::Bool:
-      return udVariant(var.toBool());
-
-    // udVariant::Type::Float
-    case QMetaType::Float:
-    case QMetaType::Double:
-      return udVariant(var.toDouble());
-
-    // udVariant::Type::Int:
-    case QMetaType::Int:
-    case QMetaType::LongLong:
-    case QMetaType::ULongLong:
-    case QMetaType::QChar:
-    case QMetaType::UInt:
-      return udVariant(var.toLongLong());
-
-    // udVariant::Type::String
-    // TODO: optimise - reduce unnecessary copies
-    case QMetaType::QByteArray:
-    case QMetaType::QString:
-      return udVariant(AllocUDStringFromQString(var.toString()), true);
-
-    // TODO: serialize other types?
-
-    // udVariant::Type::Array
-    // udVariant::Type::AssocArray
-    // udVariant::Type::Null
-    default:
-      udDebugPrintf("udToVariant: Unsupported type '%s'\n", var.typeName());
-      return udVariant();
-  };
-}
-
-inline void udFromVariant(const udVariant &variant, QVariant *pVariant)
-{
-  UDASSERT(pVariant->isNull(), "pVariant is not null");
-
-  switch (variant.type())
-  {
-    //case udVariant::Type::Null:
-    case udVariant::Type::Bool:
-      pVariant->setValue(variant.asBool());
-      break;
-
-    case udVariant::Type::Int:
-      pVariant->setValue(variant.asInt());
-      break;
-
-    case udVariant::Type::Float:
-      pVariant->setValue(variant.asFloat());
-      break;
-
-    //case udVariant::Type::Component:
-    //case udVariant::Type::Delegate:
-    // TODO: optimise?
-    case udVariant::Type::String:
-      pVariant->setValue(variant.as<QString>());
-      break;
-
-    //case udVariant::Type::Array:
-    //case udVariant::Type::AssocArray:
-    default:
-      udDebugPrintf("udToVariant: Unsupported type '%d'\n", variant.type());
-  };
-}
 
 #endif  // UDQTKERNEL_INTERNAL_H
