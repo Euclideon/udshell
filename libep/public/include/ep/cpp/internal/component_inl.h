17eee01a2ede59c9b2f3f2e577cb453bf3de1bbe
Dhiraj Pallin <dpallin@euclideon.com>
2015-12-01 12:22:22 +1000

    Fix Log() Category magic values

--- libep/public/include/ep/cpp/internal/component_inl.h
+++ libep/public/include/ep/cpp/internal/component_inl.h
@@ -44,49 +44,49 @@ template<typename ...Args>
 inline void Component::LogError(String text, Args... args) const
 {
   if (sizeof...(Args) == 0)
-    LogInternal(0, 2, text, uid);
+    LogInternal(1<<0, 2, text, uid);
   else
-    LogInternal(0, 2, MutableString128(Format, text, args...), uid);
+    LogInternal(1<<0, 2, MutableString128(Format, text, args...), uid);
 }
 template<typename ...Args>
 inline void Component::LogWarning(int level, String text, Args... args) const
 {
   if (sizeof...(Args) == 0)
-    LogInternal(1, level, text, uid);
+    LogInternal(1<<1, level, text, uid);
   else
-    LogInternal(1, level, MutableString128(Format, text, args...), uid);
+    LogInternal(1<<1, level, MutableString128(Format, text, args...), uid);
 }
 template<typename ...Args>
 inline void Component::LogDebug(int level, String text, Args... args) const
 {
   if (sizeof...(Args) == 0)
-    LogInternal(2, level, text, uid);
+    LogInternal(1<<2, level, text, uid);
   else
-    LogInternal(2, level, MutableString128(Format, text, args...), uid);
+    LogInternal(1<<2, level, MutableString128(Format, text, args...), uid);
 }
 template<typename ...Args>
 inline void Component::LogInfo(int level, String text, Args... args) const
 {
   if (sizeof...(Args) == 0)
-    LogInternal(3, level, text, uid);
+    LogInternal(1<<3, level, text, uid);
   else
-    LogInternal(3, level, MutableString128(Format, text, args...), uid);
+    LogInternal(1<<3, level, MutableString128(Format, text, args...), uid);
 }
 template<typename ...Args>
 inline void Component::LogScript(String text, Args... args) const
 {
   if (sizeof...(Args) == 0)
-    LogInternal(4, 2, text, uid);
+    LogInternal(1<<4, 2, text, uid);
   else
-    LogInternal(4, 2, MutableString128(Format, text, args...), uid);
+    LogInternal(1<<4, 2, MutableString128(Format, text, args...), uid);
 }
 template<typename ...Args>
 inline void Component::LogTrace(String text, Args... args) const
 {
   if (sizeof...(Args) == 0)
-    LogInternal(5, 2, text, uid);
+    LogInternal(1<<5, 2, text, uid);
   else
-    LogInternal(5, 2, MutableString128(Format, text, args...), uid);
+    LogInternal(1<<5, 2, MutableString128(Format, text, args...), uid);
 }
 
 } // namespace ep
