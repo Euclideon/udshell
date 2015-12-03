56aedcf28b22614826929c517f6d614b4b31cabc
Dhiraj Pallin <dpallin@euclideon.com>
2015-12-03 12:18:25 +1000

    Fix bugs in get[Left/Right]At[First/Last] functions

--- libep/public/include/ep/cpp/string.h
+++ libep/public/include/ep/cpp/string.h
@@ -71,9 +71,24 @@ struct BaseString : public Slice<const C>
   CString<C> toStringz() const;
 
   // useful functions
+
+  // make base functions local
+  size_t findFirst(C c) const                                                         { return ((Slice<const C>*)this)->findFirst(c); }
+  size_t findLast(C c) const                                                          { return ((Slice<const C>*)this)->findLast(c); }
+  size_t findFirst(BaseString<C> s) const                                             { return ((Slice<const C>*)this)->findFirst(s); }
+  size_t findLast(BaseString<C> s) const                                              { return ((Slice<const C>*)this)->findLast(s); }
+  BaseString<C> getLeftAtFirst(C c, bool bInclusive = false) const                    { return ((Slice<const C>*)this)->getLeftAtFirst(c, bInclusive); }
+  BaseString<C> getLeftAtLast(C c, bool bInclusive = false) const                     { return ((Slice<const C>*)this)->getLeftAtLast(c, bInclusive); }
+  BaseString<C> getRightAtFirst(C c, bool bInclusive = true) const                    { return ((Slice<const C>*)this)->getRightAtFirst(c, bInclusive); }
+  BaseString<C> getRightAtLast(C c, bool bInclusive = true) const                     { return ((Slice<const C>*)this)->getRightAtLast(c, bInclusive); }
+  BaseString<C> getLeftAtFirst(BaseString<C> s, bool bInclusive = false) const        { return ((Slice<const C>*)this)->getLeftAtFirst(s, bInclusive); }
+  BaseString<C> getLeftAtLast(BaseString<C> s, bool bInclusive = false) const         { return ((Slice<const C>*)this)->getLeftAtLast(s, bInclusive); }
+  BaseString<C> getRightAtFirst(BaseString<C> s, bool bInclusive = true) const        { return ((Slice<const C>*)this)->getRightAtFirst(s, bInclusive); }
+  BaseString<C> getRightAtLast(BaseString<C> s, bool bInclusive = true) const         { return ((Slice<const C>*)this)->getRightAtLast(s, bInclusive); }
+
+  // insensitice case versions
   size_t findFirstIC(BaseString<C> s) const;
   size_t findLastIC(BaseString<C> s) const;
-
   BaseString<C> getLeftAtFirstIC(BaseString<C> s, bool bInclusive = false) const;
   BaseString<C> getLeftAtLastIC(BaseString<C> s, bool bInclusive = false) const;
   BaseString<C> getRightAtFirstIC(BaseString<C> s, bool bInclusive = true) const;
@@ -151,28 +166,28 @@ struct MutableString : public Array<char, Size>
   MutableString& toLower();
 
   // c-string compatibility
-  char* toStringz(char *pBuffer, size_t bufferLen) const                                { return ((String*)this)->toStringz(pBuffer, bufferLen); }
+  char* toStringz(char *pBuffer, size_t bufferLen) const                              { return ((String*)this)->toStringz(pBuffer, bufferLen); }
   CString<char> toStringz() const;
 
   // useful functions
   size_t findFirstIC(String s) const                                                  { return ((String*)this)->findFirstIC(s); }
   size_t findLastIC(String s) const                                                   { return ((String*)this)->findLastIC(s); }
 
-  String getLeftAtFirstIC(String s, bool bInclusive = false) const                  { return ((String*)this)->getLeftAtFirstIC(s, bInclusive); }
-  String getLeftAtLastIC(String s, bool bInclusive = false) const                   { return ((String*)this)->getLeftAtLastIC(s, bInclusive); }
-  String getRightAtFirstIC(String s, bool bInclusive = true) const                  { return ((String*)this)->getRightAtFirstIC(s, bInclusive); }
-  String getRightAtLastIC(String s, bool bInclusive = true) const                   { return ((String*)this)->getRightAtLastIC(s, bInclusive); }
+  String getLeftAtFirstIC(String s, bool bInclusive = false) const                    { return ((String*)this)->getLeftAtFirstIC(s, bInclusive); }
+  String getLeftAtLastIC(String s, bool bInclusive = false) const                     { return ((String*)this)->getLeftAtLastIC(s, bInclusive); }
+  String getRightAtFirstIC(String s, bool bInclusive = true) const                    { return ((String*)this)->getRightAtFirstIC(s, bInclusive); }
+  String getRightAtLastIC(String s, bool bInclusive = true) const                     { return ((String*)this)->getRightAtLastIC(s, bInclusive); }
 
   template<bool Front = true, bool Back = true>
   String trim() const                                                                 { return ((String*)this)->trim<Front, Back>(); }
 
   template<bool skipEmptyTokens = true>
-  Slice<String> tokenise(Slice<String> tokens, String delimiters = " \t\r\n") { return ((String*)this)->tokenise<skipEmptyTokens>(tokens, delimiters); }
+  Slice<String> tokenise(Slice<String> tokens, String delimiters = " \t\r\n")         { return ((String*)this)->tokenise<skipEmptyTokens>(tokens, delimiters); }
 
-  int64_t parseInt(bool bDetectBase = true, int base = 10) const                        { return ((String*)this)->parseInt(bDetectBase, base); }
-  double parseFloat() const                                                             { return ((String*)this)->parseFloat(); }
+  int64_t parseInt(bool bDetectBase = true, int base = 10) const                      { return ((String*)this)->parseInt(bDetectBase, base); }
+  double parseFloat() const                                                           { return ((String*)this)->parseFloat(); }
 
-  uint32_t hash(uint32_t hash = 0) const                                                { return ((String*)this)->hash(hash); }
+  uint32_t hash(uint32_t hash = 0) const                                              { return ((String*)this)->hash(hash); }
 
 private:
   void appendInternal(Slice<epVarArg> args);
