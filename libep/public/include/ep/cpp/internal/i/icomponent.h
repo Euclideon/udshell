37ba78d834a8f2c5bc6fa1906455584fc80eaea4
Shane Bradley <sbradley@euclideon.com>
2016-01-13 15:40:17 +1000

    Fix access modifiers for Component's public API

--- libep/public/include/ep/cpp/internal/i/icomponent.h
+++ libep/public/include/ep/cpp/internal/i/icomponent.h
@@ -16,16 +16,20 @@ public:
   virtual Variant CallMethod(String method, Slice<const Variant> args) = 0;
   virtual void Subscribe(String eventName, const Variant::VarDelegate &delegate) = 0;
   virtual Variant Save() const = 0;
-private:
-  virtual void Init(Variant::VarMap initParams) = 0;
-  virtual epResult InitComplete() = 0;
-  virtual epResult ReceiveMessage(String message, String sender, const Variant &data) = 0;
+
   virtual void AddDynamicProperty(const PropertyInfo &property, const GetterShim *pGetter = nullptr, const SetterShim *pSetter = nullptr) = 0;
   virtual void AddDynamicMethod(const MethodInfo &method, const MethodShim *pMethod = nullptr) = 0;
   virtual void AddDynamicEvent(const EventInfo &event, const EventShim *pSubscribe = nullptr) = 0;
   virtual void RemoveDynamicProperty(String name) = 0;
   virtual void RemoveDynamicMethod(String name) = 0;
   virtual void RemoveDynamicEvent(String name) = 0;
+
+protected:
+  virtual epResult InitComplete() = 0;
+  virtual epResult ReceiveMessage(String message, String sender, const Variant &data) = 0;
+
+private:
+  virtual void Init(Variant::VarMap initParams) = 0;
 };
 
 } // namespace ep
