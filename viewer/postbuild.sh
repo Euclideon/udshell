a3a91d9f0f4a58b85305f2f95973cdca58007be2
Shane Bradley <sbradley@euclideon.com>
2016-05-30 17:28:41 +1000

    Various fixes to the premake project files.

--- viewer/postbuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ $1 = "x64" ]; then
-  cp ../ud/3rdParty/sdl2/lib/x64/SDL2.dll ./
-  cp ../3rdparty/assimp-3.1.1/lib/windows/x64/assimp-ep64.dll ./
-elif [ $1 = "x86" ]; then
-  cp ../ud/3rdParty/sdl2/lib/x86/SDL2.dll ./
-  cp ../3rdparty/assimp-3.1.1/lib/windows/x32/assimp-ep32.dll ./
-fi
