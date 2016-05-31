efb2685f2026c4f32684eaeba106c616af82052b
Erian Russell <erussell@euclideon.com>
2016-05-31 17:22:58 +1000

    Implising PrimitiveGenerator.

---
+++ kernel/src/components/primitivegeneratorimpl.h
@@ -0,0 +1,28 @@
+#pragma once
+#if !defined(_EP_PRIMITIVEGENERATORIMPL_H)
+#define _EP_PRIMITIVEGENERATORIMPL_H
+
+#include "ep/cpp/component/primitivegenerator.h"
+
+namespace ep {
+
+SHARED_CLASS(PrimitiveGenerator);
+SHARED_CLASS(ArrayBuffer);
+
+class PrimitiveGeneratorImpl : public BaseImpl<PrimitiveGenerator, IPrimitiveGenerator>
+{
+public:
+
+  PrimitiveGeneratorImpl(Component *pInstance, Variant::VarMap initParams)
+    : ImplSuper(pInstance)
+  {}
+
+  void GenerateCube(ArrayBufferRef spVB, ArrayBufferRef spIB, Delegate<Float3(Float3)> transformVertex = nullptr) override final;
+  void GenerateSphere(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments = 12, int numSlices = 6, Delegate<Float3(Float3)> transformVertex = nullptr) override final;
+  void GenerateCylinder(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments = 12, int numSlices = 1, Delegate<Float3(Float3)> transformVertex = nullptr) override final;
+  void GenerateCone(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments = 12, int numSlices = 1, Delegate<Float3(Float3)> transformVertex = nullptr) override final;
+};
+
+}
+
+#endif // _EP_PRIMITIVEGENERATORIMPL_H
