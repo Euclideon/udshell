cb25038626843f54b7e0e9c14edab4bd0210148d
Shane Bradley <sbradley@euclideon.com>
2016-06-07 14:45:26 +1000

    Fix the package build

--- build/collate.sh
+++ build/collate.sh
@@ -82,6 +82,8 @@ echo "TODO: Generate commit log..."
 if [ $BUILT_TYPE == "master" ]; then
   echo "Copy build to latest/..."
   rm -rf $PACKAGE_ROOT/old
-  mv $PACKAGE_ROOT/latest $PACKAGE_ROOT/old
+  if [ -d "$PACKAGE_ROOT/latest" ]; then
+    mv $PACKAGE_ROOT/latest $PACKAGE_ROOT/old
+  fi
   cp -r $PACKAGE_PATH $PACKAGE_ROOT/latest
 fi
