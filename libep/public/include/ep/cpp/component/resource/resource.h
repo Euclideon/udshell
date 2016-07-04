fb82f15372e24bf91b4cef7737d12d44c8ec5c5d
Erian Russell <erussell@euclideon.com>
2016-07-04 17:31:23 +1000

    Added documentation.

--- libep/public/include/ep/cpp/component/resource/resource.h
+++ libep/public/include/ep/cpp/component/resource/resource.h
@@ -18,6 +18,10 @@ public:
 
   Variant Save() const override { return pImpl->Save(); }
 
+  //! A new \c Resource instance that is a copy of this resource.
+  //! \return A copy of this resource.
+  //! \remarks Note: This method throws; it is a stub for derived classes to implement.
+  //! \see CloneResource
   virtual ResourceRef Clone() const { EPTHROW(Result::InvalidCall, "Not implemented!"); }
 
   Event<> Changed;
@@ -35,6 +39,12 @@ private:
   Array<const EventInfo> GetEvents() const;
 };
 
+//! A new \c Resource instance that is a copy of this resource.
+//! \tparam T The instance type passed and type to return.
+//! \param inst The instance to clone.
+//! \return A copy of inst.
+//! \remarks Note: This method throws if the object has not implemented Clone.
+//! \see Resource::Clone
 template <typename T, typename std::enable_if<std::is_base_of<Resource, T>::value>::type* = nullptr>
 inline SharedPtr<T> CloneResource(SharedPtr<T> inst) { return component_cast<T>(inst->Clone()); }
 
