074fbed631eab1eee208f4ecba5d358efc89eb6e
Erian Russell <erussell@euclideon.com>
2016-07-15 17:00:33 +1000

    Added documentation.

--- libep/public/include/ep/cpp/component/primitivegenerator.h
+++ libep/public/include/ep/cpp/component/primitivegenerator.h
@@ -17,6 +17,14 @@ public:
   static void GenerateQuad(ArrayBufferRef spVB, ArrayBufferRef spIB, Delegate<Float3(Float3)> transformVertex = nullptr) { GetStaticImpl()->GenerateQuad(spVB, spIB, transformVertex); }
   static void GenerateCube(ArrayBufferRef spVB, ArrayBufferRef spIB, Delegate<Float3(Float3)> transformVertex = nullptr) { GetStaticImpl()->GenerateCube(spVB, spIB, transformVertex); }
   static void GenerateSphere(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments = 12, int numSlices = 6, Delegate<Float3(Float3)> transformVertex = nullptr) { GetStaticImpl()->GenerateSphere(spVB, spIB, numSegments, numSlices, transformVertex); }
+  //! Generates a line circle model.
+  //! \param spVB The vertex buffer to fill.
+  //! \param spIB The index buffer to fill.
+  //! \param numSegments The number of line segments to draw this model with.
+  //! \param arcLength The length of the arc to draw. EP_2PI will produce a full circle.
+  //! \param transformVertex Optional function to transform the generated points.
+  //! \return None.
+  //! \remarks Note: Throws if numSegments is less than 3.
   static void GenerateCircle(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments = 12, double arcLength = EP_2PI, Delegate<Float3(Float3)> transformVertex = nullptr) { GetStaticImpl()->GenerateCircle(spVB, spIB, numSegments, arcLength, transformVertex); }
   static void GenerateCylinder(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments = 12, int numSlices = 1, Delegate<Float3(Float3)> transformVertex = nullptr) { GetStaticImpl()->GenerateCylinder(spVB, spIB, numSegments, numSlices, transformVertex); }
   static void GenerateCone(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments = 12, int numSlices = 1, Delegate<Float3(Float3)> transformVertex = nullptr) { GetStaticImpl()->GenerateCone(spVB, spIB, numSegments, numSlices, transformVertex); }
