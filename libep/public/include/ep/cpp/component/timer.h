2ec73e2d157a4700f12b73c9fdb59cab7c8189ad
Manu Evans <mevans@euclideon.com>
2016-07-20 14:17:18 +1000

    Doco

--- libep/public/include/ep/cpp/component/timer.h
+++ libep/public/include/ep/cpp/component/timer.h
@@ -14,14 +14,33 @@ class Timer : public Component
   EP_DECLARE_COMPONENT_WITH_IMPL(ep, Timer, ITimer, Component, EPKERNEL_PLUGINVERSION, "Timer desc...", 0)
 public:
 
+  //! Get the currently configured timer duration.
+  //! \return The duration of the timer in seconds.
+  //! \see GetTimerType
   double GetDuration() const { return pImpl->GetDuration(); };
+
+  //! Get the currently configured timer type.
+  //! \return The type of the timer.
+  //! \see GetDuration
   TimerType GetTimerType() const { return pImpl->GetTimerType(); }
 
+  //! Reset the timer using the currently configured settings.
+  //! \return None.
   void Reset() { pImpl->Reset(); }
 
+  //! Begin an interval counter. The \c Elapsed signal will trigger each \a interval seconds.
+  //! \param interval The timer interval in seconds.
+  //! \return None.
+  //! \see BeginCountdown
   void BeginInterval(double interval) { pImpl->BeginInterval(interval); }
+
+  //! Begin a countdown. The \c Elapsed signal will trigger after the countdown completes.
+  //! \param duration The countdown duration in seconds.
+  //! \return None.
+  //! \see BeginInterval
   void BeginCountdown(double duration) { pImpl->BeginCountdown(duration); }
 
+  //! Event that triggers when the timer elapses.
   Event<> Elapsed;
 
 private:
