f1f9c1080aadf54c9be585837b366187a968e2e4
Erian Russell <erussell@euclideon.com>
2016-07-22 10:55:27 +1000

    Added documentation.

--- libep/public/include/ep/cpp/component/primitivegenerator.h
+++ libep/public/include/ep/cpp/component/primitivegenerator.h
@@ -26,6 +26,15 @@ public:
   //! \return None.
   //! \remarks Note: Throws if numSegments is less than 3.
   static void GenerateCircle(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments = 12, double arcLength = EP_2PI, Delegate<Float3(Float3)> transformVertex = nullptr) { GetStaticImpl()->GenerateCircle(spVB, spIB, numSegments, arcLength, transformVertex); }
+  //! Generates a disc model.
+  //! \param spVB The vertex buffer to fill.
+  //! \param spIB The index buffer to fill.
+  //! \param numSegments The number of line segments to draw this model with.
+  //! \param arcLength The length of the arc to draw. EP_2PI will produce a full disc.
+  //! \param innerRadius The inner radius to produce the disc from; 0 will make a solid disc.
+  //! \param transformVertex Optional function to transform the generated points.
+  //! \return None.
+  //! \remarks Note: Throws if numSegments is less than 3.
   static void GenerateDisc(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments = 12, double arcLength = EP_2PI, double innerRadius = 0, Delegate<Float3(Float3)> transformVertex = nullptr) { GetStaticImpl()->GenerateDisc(spVB, spIB, numSegments, arcLength, innerRadius, transformVertex); }
   static void GenerateCylinder(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments = 12, int numSlices = 1, Delegate<Float3(Float3)> transformVertex = nullptr) { GetStaticImpl()->GenerateCylinder(spVB, spIB, numSegments, numSlices, transformVertex); }
   static void GenerateCone(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments = 12, int numSlices = 1, Delegate<Float3(Float3)> transformVertex = nullptr) { GetStaticImpl()->GenerateCone(spVB, spIB, numSegments, numSlices, transformVertex); }
