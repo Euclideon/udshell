3f3146aee3e778dd9128ce7340c4828d135b842f
Erian Russell <erussell@euclideon.com>
2016-07-22 10:52:09 +1000

    Removed default parameters.

--- libep/public/include/ep/cpp/internal/i/iprimitivegenerator.h
+++ libep/public/include/ep/cpp/internal/i/iprimitivegenerator.h
@@ -16,12 +16,12 @@ class IPrimitiveGenerator
 class IPrimitiveGeneratorStatic
 {
 public:
-  virtual void GenerateQuad(ArrayBufferRef spVB, ArrayBufferRef spIB, Delegate<Float3(Float3)> transformVertex = nullptr) = 0;
-  virtual void GenerateCube(ArrayBufferRef spVB, ArrayBufferRef spIB, Delegate<Float3(Float3)> transformVertex = nullptr) = 0;
-  virtual void GenerateSphere(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments = 12, int numSlices = 6, Delegate<Float3(Float3)> transformVertex = nullptr) = 0;
+  virtual void GenerateQuad(ArrayBufferRef spVB, ArrayBufferRef spIB, Delegate<Float3(Float3)> transformVertex) = 0;
+  virtual void GenerateCube(ArrayBufferRef spVB, ArrayBufferRef spIB, Delegate<Float3(Float3)> transformVertex) = 0;
+  virtual void GenerateSphere(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments, int numSlices, Delegate<Float3(Float3)> transformVertex) = 0;
   virtual void GenerateCircle(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments, double arcLength, Delegate<Float3(Float3)> transformVertex) = 0;
-  virtual void GenerateCylinder(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments = 12, int numSlices = 1, Delegate<Float3(Float3)> transformVertex = nullptr) = 0;
-  virtual void GenerateCone(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments = 12, int numSlices = 1, Delegate<Float3(Float3)> transformVertex = nullptr) = 0;
+  virtual void GenerateCylinder(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments, int numSlices, Delegate<Float3(Float3)> transformVertex) = 0;
+  virtual void GenerateCone(ArrayBufferRef spVB, ArrayBufferRef spIB, int numSegments, int numSlices, Delegate<Float3(Float3)> transformVertex) = 0;
 };
 
 }
